dbae5d69bf49e534ed905dbf7c20839d6ad1433d
Your Name <HarvyAlbarran>
2021-12-10 08:26:00 -0500

    Finished

--- conexion_global/r_conexion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-    $mysqli = new mysqli('localhost', 'root', '', 'jhayli');
-    if (mysqli_connect_errno()) {
-        echo 'La conexion fallida: ',mysqli_connect_error();
-        exit();
-    }
-
-?>
